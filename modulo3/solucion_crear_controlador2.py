f0f09a149a39dce7e9159a2456c2670c8265436e
Carlos <caedo.00@gmail.com>
2025-05-11 12:16:38 -0600

    Documentar solucion_crear_controlador2.py

--- modulo3/solucion_crear_controlador2.py
+++ modulo3/solucion_crear_controlador2.py
@@ -1,3 +1,13 @@
+"""
+****************************************************************************************************
+     TODOS LOS EJEMPLOS Y SOLUCIONES ESTÁN LISTOS PARA COPIAR Y PEGARSE EN EL SCRIPT EDITOR
+****************************************************************************************************
+
+Este ejemplo muestra cómo crear un controlador FK para un joint rotando los vértices del controlador.
+
+Los pasos de script son casi idénticos a los pasos descritos en la solución que utiliza el parámetro `normal(nr)`.
+Refiérase al archivo `solucion_crear_controlador.py`.
+"""
 import maya.cmds as cmds
   
 targetJoint = cmds.ls(sl=True)[0]
@@ -15,6 +25,14 @@ print("Positioning controller")
 pointConst = cmds.parentConstraint(targetJoint, groupName, weight=1)[0]
 cmds.delete(pointConst)
 
+# Se deselecciona los objetos que están actualmente seleccionados.
+# Debemos seleccionar solamente los vértices del controlador, no el controlador en sí,
+# para no alterar los valores del controlador. Se usan los índices que tienen los vértices.
+# En este caso se selecciona un rango del vértice 0 al 7.
+# Luego esos vértices se rotan 90 grados en el eje Z. Finalmente, se deseleccionan.
+# Si usted selecciona los vértices manualmente, script editor va a mostrar
+# resultados parecidos a este snippet.
+# Lo mas importante de este snippet es la forma en que se seleccionan vértices con Python.
 print("Rotate controller's vertices")
 cmds.select(cl=True)
 cmds.select(newControllerName + ".cv[0:7]")
