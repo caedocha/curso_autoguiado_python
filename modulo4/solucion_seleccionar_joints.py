092be5f2075529a32d961c92b94674c214b5df7c
Carlos <caedo.00@gmail.com>
2025-05-17 17:29:16 -0600

    Documentar seleccionar joints

--- modulo4/solucion_seleccionar_joints.py
+++ modulo4/solucion_seleccionar_joints.py
@@ -1,15 +1,32 @@
+"""
+****************************************************************************************************
+     TODOS LOS EJEMPLOS Y SOLUCIONES ESTÁN LISTOS PARA COPIAR Y PEGARSE EN EL SCRIPT EDITOR
+****************************************************************************************************
+
+Este ejemplo muestra la solución para filtrar solamente los joints de una selección de objetos más grande.
+"""
 import maya.cmds as cmds
 
+# Crea la lista que va a guardar los nombres de los joints.
 joints = []
+
+# Lista todos los objetos seleccionados, de cualquier tipo.
 all_objs = cmds.ls(sl=True)
 
+# Itera sobre los objetos seleccionados.
 for obj in all_objs:
+    # Valida si el tipo del objeto es "joint".
     if(cmds.nodeType(obj) == "joint"):
+        #  Si es True, lo agrega en la lista de joints.
         joints.append(obj)
 
-cmds.select(cl=True) # Clear the original selection.
+# Limpia la selección de todos los objetos.
+cmds.select(cl=True)
 
+# Valida si hay joints en la lista de joints.
 if(len(joints) > 0):
+    # Si es True, selecciona esos joints.
     cmds.select(joints)
 else:
+    # Si es False, muestra un mensaje de retroalimentación en el script editor.
     print("Whoops! There are no joints in the selection.")
