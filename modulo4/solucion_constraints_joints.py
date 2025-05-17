dddc52962aaa15e82ae207b7420fc9dcc27fd842
Carlos <caedo.00@gmail.com>
2025-05-17 14:48:49 -0600

    Documentar soluciones mod4

--- modulo4/solucion_constraints_joints.py
+++ modulo4/solucion_constraints_joints.py
@@ -1,10 +1,29 @@
+"""
+****************************************************************************************************
+     TODOS LOS EJEMPLOS Y SOLUCIONES ESTÁN LISTOS PARA COPIAR Y PEGARSE EN EL SCRIPT EDITOR
+****************************************************************************************************
+
+Este ejemplo muestra la solución para crear los constraints entre las cadenas de joints IK y FK y la cadena de bind.
+"""
 import maya.cmds as cmds
 
+# Se asume que los joints siguen una nomenclatura definida por uno como rigger.
 joints = ["jnt_r_shoulder", "jnt_r_elbow", "jnt_r_wrist"]
 
+# Se itera sobre la lista de `joints`.
 print("Creating parent constraints")
 for jnt in joints:
-    print("Creating constraints for " + jnt.split("_")[2])
-    cmds.parentConstraint(jnt + "_ik", jnt + "_bind")
-    cmds.parentConstraint(jnt + "_fk", jnt + "_bind")
+    # Se divide el nombre del joint por el carácter "_".
+    # La nomenclatura que se usa en este ejemplo, dicta que en el índice 2 se guarda la parte del cuerpo.
+    # Por ejemplo, shoulder, elbow, wrist.
+    body_part = jnt.split("_")[2]
+    print("Creating constraints for " + body_part)
+    # Se declaran los nombres de los joints IK, FK y bind.
+    ik_jnt = jnt + "_ik"
+    fk_jnt = jnt + "_fk"
+    bind_jnt = jnt + "_bind"
+    # Se crea el parent constraint entre IK y bind.
+    cmds.parentConstraint(ik_jnt, bind_jnt)
+    # Se crea el parent constraint entre FK y bind.
+    cmds.parentConstraint(fk_jnt, bind_jnt)
 print("Done")
--- modulo4/solucion_ik_rig.py
+++ modulo4/solucion_ik_rig.py
@@ -1,47 +1,66 @@
+"""
+****************************************************************************************************
+     TODOS LOS EJEMPLOS Y SOLUCIONES ESTÁN LISTOS PARA COPIAR Y PEGARSE EN EL SCRIPT EDITOR
+****************************************************************************************************
+
+Este ejemplo muestra la solución para crear el rig IK de la cadena de joints de un brazo.
+"""
 import maya.cmds as cmds
 
-# Initial 3 joints
+# Los nombres de los joints.
 shoulder_joint = "jnt_r_shoulder_ik"
 elbow_joint = "jnt_r_elbow_ik"
 wrist_joint = "jnt_r_wrist_ik"
 
-# Name of the controllers and the controllers' groups
+# Los nombres de los controladores y los grupos de los controladores.
 pole_vector_controller = "poleVector_r_ctrl"
 pole_vector_controller_group = pole_vector_controller + "_grp"
 ik_controller = "ik_r_ctrl"
 ik_controller_group = ik_controller + "_grp"
 
 print("Creating IK handle")
-ik_handle = cmds.ikHandle( sj=shoulder_joint, ee=wrist_joint)[0]
+# Se crea el IK handle usando el joint del hombro y la muñeca.
+# Luego se guarda el nombre del IK handle en la variable `ik_handle`.
+# Note que la funcion ikHandle devuelve una lista de objetos,
+# el primero es el nombre del IK handle.
+ik_handle = cmds.ikHandle(sj=shoulder_joint, ee=wrist_joint)[0]
 print(ik_handle)
 
 print("Creating pole vector controller")
+# Crea un locator para que sea el controlador del pole vector.
 cmds.spaceLocator(n=pole_vector_controller)
 
 print("Grouping pole vector controller")
+# Crea un grupo vacío y luego "parentea" el controlador del pole vector.
 cmds.group(em=True, n=pole_vector_controller_group)
 cmds.parent(pole_vector_controller, pole_vector_controller_group)
 
 print("Positioning pole vector controller")
+# Posiciona el pole vector sobre el joint de hombro y lo mueve un poco para atrás.
 pointConst = cmds.pointConstraint(elbow_joint, pole_vector_controller_group, weight=1)[0]
 cmds.delete(pointConst)
 cmds.move(0,0,-2, pole_vector_controller_group, relative=True)
 
 print("Creating pole vector constraint")
+# Crea el pole vector.
 cmds.poleVectorConstraint(pole_vector_controller, ik_handle)
 
 print("Creating IK controller")
+# Crea el controlador IK.
 cmds.circle(c=(0,0,0), nr=(1,0,0), r=1, d=3, ut=0, tol=0.1, s=8, ch=1, name=ik_controller)
 
 print("Grouping up IK controller")
+# Crea un grupo vacío y "parentea" el controlador con ese grupo.
 cmds.group(em=True, n=ik_controller_group)
 cmds.parent(ik_controller, ik_controller_group)
 
 print("Positioning IK controller")
+# Posiciona el controlador IK sobre el joint de la muñeca.
 pointConst = cmds.parentConstraint(wrist_joint, ik_controller_group, weight=1)[0]
 cmds.delete(pointConst)
 
 print("Parenting IK handle to IK controller")
+# "Parentea" el IK handle con el controlador IK.
 cmds.parent(ik_handle, ik_controller)
 
 print("Done")
