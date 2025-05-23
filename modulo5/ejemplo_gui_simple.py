9dfd013d56d04cdbee9a2db2c8cf162383c0a47a
Carlos <caedo.00@gmail.com>
2025-05-23 05:42:58 -0600

    Agregar ejemplo gui simple

---
+++ modulo5/ejemplo_gui_simple.py
@@ -0,0 +1,38 @@
+"""
+****************************************************************************************************
+     TODOS LOS EJEMPLOS Y SOLUCIONES ESTÁN LISTOS PARA COPIAR Y PEGARSE EN EL SCRIPT EDITOR
+****************************************************************************************************
+
+Ejemplo para crear una interfaz gráfica simple.
+"""
+
+import maya.cmds as cmds
+
+def main():
+    print("Creating simple UI")
+
+    # Las variables de los nombres de los controles para poder identificarlos.
+    sliderName = "slider"
+    textboxName = "textbox"
+    checkboxName = "checkbox"
+
+    # La ventana es el contendor principal. Siempre hay que empezar creándola.
+    cpwin = cmds.window(title="Simple UI",rtf=True)
+
+    # Se crea un layout de columnas. Noten que para asoaciarlo a la ventana, al layout
+    # se le pasa el párametro parent(p) con el nombre de la ventana.
+    # Esto indica que layout va adentro de la ventana.
+    layout = cmds.columnLayout(p=cpwin, w=300, h=200,cat=("left",10),rs=10)
+
+    # Se crean controladores de varios tipos. Noten que a los controles se les indica
+    # a quién pertenecen con el parámetro parent(p) pero en este caso el "papá" es el layout.
+    cmds.floatSliderGrp(sliderName, p=layout,label="Slider",w=280,min=0,max=100,value=50,step=1,field=True,cal=[1,"left"])
+    cmds.textFieldGrp(textboxName, p=layout,h=50,label="Textbox",w=280,cal=[1,"left"])
+    cmds.checkBox(checkboxName,p=layout,l="Checkbox",w=280,enable=True)
+    cmds.button(p=layout,h=50,w=280,l="Button")
+
+    # Después de agregar el layout y los controles, se muestra la ventana.
+    cmds.showWindow(cpwin)
+    print("Done")
+
+main()
